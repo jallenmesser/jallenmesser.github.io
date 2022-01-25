6bf803dfd84af6ea96cc037e955719a8001b60d7
Jallen Messersmith <35847263+jallenmesser@users.noreply.github.com>
2022-01-25 17:39:39 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
-window.onload = function() {
-    // similar behavior as clicking on a link
-    window.location.href = "http://stackoverflow.com";
-}
+// window.onload = function() {
+//     // similar behavior as clicking on a link
+//     window.location.href = "http://stackoverflow.com";
+// }
